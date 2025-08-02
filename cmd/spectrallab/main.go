0de98df73f617074e01dbb4edbee2b81b0af9ce4
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-02 21:27:25 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/spectrallab/main.go
@@ -0,0 +1,23 @@
+// cmd/spectrallab/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"spectrallab/internal/spectrallab"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := spectrallab.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
